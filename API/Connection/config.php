0de878cf2910479bc999a0e2404f9de51982afaf
Ridmal <rajeewaakmeemana@gmail.com>
2026-03-11 07:37:31 +0530

    Production Release 2026.03.11

--- API/Connection/config.php
+++ API/Connection/config.php
@@ -8,8 +8,8 @@
 	}
 
 	// TinyMCE API Key UAT
-	define("TINYMCE_API_KEY", "9lf9h735jucnqfgf4ugu8egij1icgzsrgbcmsk5tg44cjba8");
+	// define("TINYMCE_API_KEY", "9lf9h735jucnqfgf4ugu8egij1icgzsrgbcmsk5tg44cjba8");
 	
 	// TinyMCE API Key PROD
-	// define("TINYMCE_API_KEY", "2d97jyc4gvuspgz5colxqhp9cfvv2ob07mrjmmtat4v9cd7c");
+	define("TINYMCE_API_KEY", "2d97jyc4gvuspgz5colxqhp9cfvv2ob07mrjmmtat4v9cd7c");
 ?>
