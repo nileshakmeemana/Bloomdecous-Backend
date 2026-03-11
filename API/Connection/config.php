462f27dffd5fbd5ee9c589d145898ae2d8de8bf4
Ridmal <rajeewaakmeemana@gmail.com>
2026-03-11 06:38:07 +0530

    UAT Release 2026.03.11

--- API/Connection/config.php
+++ API/Connection/config.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect("localhost", "root", "", "db_bloomdecous");
+	// $conn = mysqli_connect("localhost", "root", "", "db_bloomdecous");
 	// $conn = mysqli_connect("localhost", "mdrizxbowc_bloomdeco", "I~mqQ26*~URT", "mdrizxbowc_db_bloomdecous");
 	$conn = mysqli_connect("localhost", "mdrizxbowc_bloomdeco_uat", "anJ*f$72[R_#", "mdrizxbowc_db_bloomdecous_uat");
 	
