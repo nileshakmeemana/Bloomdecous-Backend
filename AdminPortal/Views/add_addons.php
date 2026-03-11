e944ed99aa76471841bf9dc03058b12f3e49d317
Ridmal <rajeewaakmeemana@gmail.com>
2026-03-12 00:01:52 +0530

    Production Release 2026.03.12

--- AdminPortal/Views/add_addons.php
+++ AdminPortal/Views/add_addons.php
@@ -49,7 +49,7 @@ if (mysqli_num_rows($permission_query) > 0) {
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-	<title><?php echo ($companyName); ?> - Packages</title>
+	<title><?php echo ($companyName); ?> - Addons</title>
 
 	<!-- Favicon -->
 	<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
